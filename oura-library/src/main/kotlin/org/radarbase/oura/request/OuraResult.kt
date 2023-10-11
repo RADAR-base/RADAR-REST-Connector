070c98aa371d8b5ad073d7b43d25a948990fcf9d
Pauline <mpgxvii@gmail.com>
2023-10-11 16:00:47 +0100

    Add custom Oura errors and fix error handling

--- oura-library/src/main/kotlin/org/radarbase/oura/request/OuraResult.kt
+++ oura-library/src/main/kotlin/org/radarbase/oura/request/OuraResult.kt
@@ -10,3 +10,20 @@ sealed interface OuraError
 sealed class OuraErrorBase(val message: String, val cause: Exception? = null) : OuraError
 
 class OuraRateLimitError : OuraErrorBase("Rate limit reached..", TooManyRequestsException())
+
+class OuraClientException : OuraErrorBase(
+    "Client unsupported or unauthorized..",
+    TooManyRequestsException(),
+)
+
+class OuraUnauthorizedAccessError : OuraErrorBase(
+    "Access token expired or revoked..",
+    TooManyRequestsException(),
+)
+
+class OuraAccessForbiddenError : OuraErrorBase(
+    "Oura subscription has expired or API data not available..",
+    TooManyRequestsException(),
+)
+
+class OuraValidationError : OuraErrorBase("Invalid Oura data..", TooManyRequestsException())
