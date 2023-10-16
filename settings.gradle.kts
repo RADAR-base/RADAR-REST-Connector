ba50d1a07ecae995742decfbe87fdb57491fba66
Joris Borgdorff <joris@thehyve.nl>
2023-10-16 13:59:07 +0200

    Bump version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,17 +6,5 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven(url = "https://maven.pkg.github.com/radar-base/radar-commons") {
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                    ?: extra.properties["gpr.user"] as? String
-                    ?: extra.properties["public.gpr.user"] as? String
-                password = System.getenv("GITHUB_TOKEN")
-                    ?: extra.properties["gpr.token"] as? String
-                    ?: (extra.properties["public.gpr.token"] as? String)?.let {
-                        java.util.Base64.getDecoder().decode(it).decodeToString()
-                    }
-            }
-        }
     }
 }
