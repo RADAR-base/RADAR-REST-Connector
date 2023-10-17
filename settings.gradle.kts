e5668debc5bc3b506f98d521988d572d2b5e395a
Pauline <mpgxvii@gmail.com>
2023-10-17 16:55:55 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,7 @@
 rootProject.name = "kafka-connect-rest-source"
 include(":kafka-connect-fitbit-source")
 include(":kafka-connect-rest-source")
+include(":kafka-connect-oura-source")
 include(":oura-library")
 
 pluginManagement {
