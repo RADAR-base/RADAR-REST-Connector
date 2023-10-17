624e36596b0c06e3adf162852d34da5e0a6ad114
Joris Borgdorff <joris@thehyve.nl>
2023-10-17 17:10:22 +0200

    Removed oauth-client-util dependency and replaced with ktor

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -17,10 +17,11 @@ object Versions {
 
     const val okhttp = "4.11.0"
 
-    const val managementPortal = "2.0.0"
+    const val managementPortal = "2.1.0"
 
     const val firebaseAdmin = "9.1.0"
     const val radarSchemas = "0.8.4"
+    const val ktor = "2.3.5"
 
     const val junit = "5.9.3"
     const val wiremock = "2.27.2"
--- kafka-connect-fitbit-source/build.gradle.kts
+++ kafka-connect-fitbit-source/build.gradle.kts
@@ -4,12 +4,17 @@ dependencies {
     api(project(":kafka-connect-rest-source"))
     api("io.confluent:kafka-connect-avro-converter:${Versions.confluent}")
     api("org.radarbase:radar-schemas-commons:${Versions.radarSchemas}")
-    implementation("org.radarbase:oauth-client-util:${Versions.managementPortal}")
+    implementation("org.radarbase:radar-commons-kotlin:${Versions.radarCommons}")
 
     implementation(platform("com.fasterxml.jackson:jackson-bom:${Versions.jackson}"))
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
     implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
     implementation("com.google.firebase:firebase-admin:${Versions.firebaseAdmin}")
+
+    implementation("io.ktor:ktor-client-auth:${Versions.ktor}")
+    implementation("io.ktor:ktor-client-content-negotiation:${Versions.ktor}")
+    implementation("io.ktor:ktor-serialization-jackson:${Versions.ktor}")
+    implementation("io.ktor:ktor-client-cio-jvm:${Versions.ktor}")
 
     // Included in connector runtime
     compileOnly("org.apache.kafka:connect-api:${Versions.kafka}")
