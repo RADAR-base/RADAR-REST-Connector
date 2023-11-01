e2eff497935bf4e9dc6f5244f53795913494dac4
Pauline <mpgxvii@gmail.com>
2023-11-01 17:54:52 +0000

    Fix OuraServiceUserRepository serialization issues

--- kafka-connect-oura-source/src/main/java/org/radarbase/connect/rest/oura/user/OuraServiceUserRepository.kt
+++ kafka-connect-oura-source/src/main/java/org/radarbase/connect/rest/oura/user/OuraServiceUserRepository.kt
@@ -16,9 +16,11 @@
  */
 package org.radarbase.connect.rest.oura.user
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.auth.Auth
@@ -41,9 +43,11 @@ import io.ktor.http.contentType
 import io.ktor.http.isSuccess
 import io.ktor.http.takeFrom
 import io.ktor.serialization.jackson.jackson
+import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
+import kotlinx.serialization.json.Json
 import org.radarbase.connect.rest.oura.OuraRestSourceConnectorConfig
 import org.radarbase.kotlin.coroutines.CacheConfig
 import org.radarbase.kotlin.coroutines.CachedSet
@@ -69,6 +73,7 @@ class OuraServiceUserRepository : UserRepository {
     private val credentialCaches = ConcurrentHashMap<String, CachedValue<OAuth2UserCredentials>>()
     private val credentialCacheConfig =
         CacheConfig(refreshDuration = 1.days, retryDuration = 1.minutes)
+    private val mapper = ObjectMapper().registerKotlinModule().registerModule(JavaTimeModule())
 
     @Throws(IOException::class)
     override fun get(key: String): User = runBlocking(Dispatchers.Default) {
@@ -86,7 +91,6 @@ class OuraServiceUserRepository : UserRepository {
             clientId = config.ouraUserRepositoryClientId,
             clientSecret = config.ouraUserRepositoryClientSecret,
         )
-        
 
         userCache = CachedSet(
             CacheConfig(refreshDuration = 1.hours, retryDuration = 1.minutes),
@@ -94,10 +98,10 @@ class OuraServiceUserRepository : UserRepository {
             makeRequest<OuraUsers> { url("users?source-type=Oura") }
                 .users
                 .toHashSet()
-            // .filterTo(HashSet()) { u ->
-            //     u.isComplete() &&
-            //         (containedUsers.isEmpty() || u.versionedId in containedUsers)
-            // }
+                .filterTo(HashSet()) { u ->
+                    u.isComplete() &&
+                        (containedUsers.isEmpty() || u.versionedId in containedUsers)
+                }
         }
     }
 
@@ -116,6 +120,13 @@ class OuraServiceUserRepository : UserRepository {
                         clientSecret,
                     ).copyWithEnv("MANAGEMENT_PORTAL"),
                     baseUrl.host,
+                )
+            }
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                    },
                 )
             }
         } else if (clientId != null && clientSecret != null) {
@@ -244,7 +255,7 @@ class OuraServiceUserRepository : UserRepository {
             }
             throw HttpResponseException(message, response.status.value)
         }
-        response.body<T>()
+        mapper.readValue<T>(response.bodyAsText())
     }
 
     companion object {
