c7450fd3f64d7b5935047b84610b4d94797b5ea1
Pauline <mpgxvii@gmail.com>
2024-04-17 16:52:14 +0100

    Bump deps to latest versions and bump java to 17

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,24 +2,24 @@
 object Versions {
     const val project = "0.4.2-SNAPSHOT"
 
-    const val java = 11
+    const val java = 17
     const val kotlin = "1.9.10"
     const val wrapper = "8.4"
 
-    const val radarCommons = "1.1.1"
-    const val confluent = "7.5.0"
+    const val radarCommons = "1.1.2"
+    const val confluent = "7.6.0"
     const val kafka = "$confluent-ce"
-    const val avro = "1.11.0"
+    const val avro = "1.11.3"
 
     const val managementPortal = "2.0.0"
 
     // From image
-    const val jackson = "2.14.2"
+    const val jackson = "2.16.1"
 
     const val log4j2 = "2.20.0"
     const val slf4j = "2.0.9"
 
-    const val okhttp = "4.11.0"
+    const val okhttp = "4.12.0"
 
     const val firebaseAdmin = "9.1.0"
     const val radarSchemas = "0.8.7-hotfix"
@@ -29,5 +29,5 @@ object Versions {
     const val wiremock = "2.27.2"
     const val mockito = "5.3.1"
 
-    const val kotlinVersion = "1.8.21"
+    const val kotlinVersion = "1.9.10"
 }
