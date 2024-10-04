ec081cb0f187387e53477f62a60a8ff07c1d92c4
pvannierop <pim@thehyve.nl>
2024-10-04 16:23:16 +0200

    Add support for Sentry monitoring
    Includes addition to documentation.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -6,7 +6,7 @@ object Versions {
     const val kotlin = "1.9.22"
     const val wrapper = "8.4"
 
-    const val radarCommons = "1.1.2"
+    const val radarCommons = "1.1.3"
     const val confluent = "7.7.0"
     const val kafka = "$confluent-ce"
     const val avro = "1.12.0"
@@ -18,6 +18,7 @@ object Versions {
 
     const val log4j2 = "2.23.1"
     const val slf4j = "2.0.13"
+    const val sentryLog4j = "1.7.30"
 
     const val okhttp = "4.12.0"
 
