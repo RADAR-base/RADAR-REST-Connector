086cb3a64a12fae83b49a7603df1b55ffcdbe7d2
pvannierop <pim@thehyve.nl>
2024-10-18 07:19:00 +0200

    Up radar-schemas version to 0.8.11

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -22,7 +22,7 @@ object Versions {
     const val okhttp = "4.12.0"
 
     const val firebaseAdmin = "9.2.0"
-    const val radarSchemas = "0.8.7-hotfix"
+    const val radarSchemas = "0.8.11"
     const val ktor = "2.3.10"
 
     const val junit = "5.10.2"
