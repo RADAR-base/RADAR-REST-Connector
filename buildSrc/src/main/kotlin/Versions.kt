a88b53c4723a748bc3c99222de11f1287b905592
pvannierop <pim@thehyve.nl>
2024-10-18 07:19:59 +0200

    Set project version to 0.5.4

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "0.5.3-SNAPSHOT"
+    const val project = "0.5.4"
 
     const val java = 17
     const val kotlin = "1.9.22"
