ccdf1194b91691cb77a9bda0f0afae03a32bd571
Pauline <mpgxvii@gmail.com>
2025-06-17 12:18:28 +0100

    Bump snapshot version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "0.6.2"
+    const val project = "0.6.3-SNAPSHOT"
 
     const val java = 17
     const val kotlin = "1.9.22"
