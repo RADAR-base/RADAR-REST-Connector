69c2e29b46a2d04228c379f2e71779532db74d46
Pauline Conde <mpgxvii@gmail.com>
2025-06-20 15:30:36 +0100

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "0.7.0"
+    const val project = "0.7.0-SNAPSHOT"
 
     const val java = 17
     const val kotlin = "1.9.22"
