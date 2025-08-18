5a4ff751cdf33fb567a63e1452cc83cefc340a06
Pauline <mpgxvii@gmail.com>
2025-08-18 10:41:27 +0100

    Update Oura heart rate route to request daily

--- oura-library/src/main/kotlin/org/radarbase/oura/route/OuraHeartRateRoute.kt
+++ oura-library/src/main/kotlin/org/radarbase/oura/route/OuraHeartRateRoute.kt
@@ -1,7 +1,15 @@
 package org.radarbase.oura.route
 
 import org.radarbase.oura.converter.OuraHeartRateConverter
+import org.radarbase.oura.user.User
 import org.radarbase.oura.user.UserRepository
+import org.radarcns.connector.oura.OuraHeartRate
+import org.radarcns.connector.oura.OuraHeartRateSource
+import org.slf4j.LoggerFactory
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDateTime
+import org.radarbase.oura.request.RestRequest
 
 class OuraHeartRateRoute(
     private val userRepository: UserRepository,
@@ -12,4 +20,37 @@ class OuraHeartRateRoute(
     override fun toString(): String = "oura_heart_rate"
 
     override var converters = listOf(OuraHeartRateConverter())
+
+    override var maxIntervalPerRequest = Duration.ofDays(1)
+
+    var maxRequestsPerInterval = 1000
+
+    override fun generateRequests(
+        user: User,
+        start: Instant,
+        end: Instant,
+    ): Sequence<RestRequest> {
+        return generateRequests(user, start, end, maxRequestsPerInterval)
+    }
+
+    override fun generateRequests(
+        user: User,
+        start: Instant,
+        end: Instant,
+        max: Int,
+    ): Sequence<RestRequest> {
+        return generateSequence(start) { it + maxIntervalPerRequest }
+            .takeWhile { it < end }
+            .take(maxRequestsPerInterval)
+            .map { startRange ->
+                val endRange = (startRange + maxIntervalPerRequest).coerceAtMost(end)
+                val request = createRequest(
+                    user,
+                    "$OURA_API_BASE_URL/${subPath()}",
+                    "?start_datetime=${startRange.toLocalDate()}" +
+                        "&end_datetime=${endRange.toLocalDate()}",
+                )
+                RestRequest(request, user, this, startRange, endRange)
+            }
+    }
 }
