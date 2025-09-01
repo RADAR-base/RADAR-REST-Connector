6aaf7c61724f454e6da462b1256abb9088ae0ca2
Pim van Nierop <pim@thehyve.nl>
2025-09-01 15:19:29 +0200

    Apply security upgrades October 2025 platform upgrade

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,17 +1,17 @@
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "0.7.0"
+    const val project = "0.6.3"
 
     const val java = 17
     const val kotlin = "1.9.22"
     const val wrapper = "8.9"
 
-    const val radarCommons = "1.2.2"
+    const val radarCommons = "1.2.4"
     const val confluent = "7.8.1"
     const val kafka = "$confluent-ce"
     const val avro = "1.12.0"
 
-    const val managementPortal = "2.0.0"
+    const val managementPortal = "2.1.10"
 
     // From image
     const val jackson = "2.17.0"
@@ -19,7 +19,6 @@ object Versions {
     const val log4j2 = "2.23.1"
     const val slf4j = "2.0.13"
     const val sentryLog4j = "1.7.30"
-    const val sentryOpenTelemetryAgent = "8.1.0"
 
     const val okhttp = "4.12.0"
 
