95b08434c20f769d7c593eade97559a707488046
Pim van Nierop <pim@thehyve.nl>
2025-09-03 08:27:08 +0200

    Up project version to 0.7.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 @Suppress("ConstPropertyName", "MemberVisibilityCanBePrivate")
 object Versions {
-    const val project = "0.6.4"
+    const val project = "0.7.1"
 
     const val java = 17
     const val kotlin = "1.9.22"
