2f64a7bd8c64bba68eb69b7eed14eb0d2ec16d5e
this-Aditya <adityamishra2217@gmail.com>
2026-05-01 14:00:52 +0530

    Include new library as a project sub-module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ include(":kafka-connect-fitbit-source")
 include(":kafka-connect-rest-source")
 include(":kafka-connect-oura-source")
 include(":oura-library")
+include(":google-health-library")
 
 pluginManagement {
     repositories {
