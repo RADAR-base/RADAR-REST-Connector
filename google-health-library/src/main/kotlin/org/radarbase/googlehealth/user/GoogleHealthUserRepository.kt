66cc5f143ae4efd0772daf811094b5ff099c4b92
this-Aditya <adityamishra2217@gmail.com>
2026-06-08 02:16:23 +0530

    Abstract method for fetch unauthorized users

--- google-health-library/src/main/kotlin/org/radarbase/googlehealth/user/GoogleHealthUserRepository.kt
+++ google-health-library/src/main/kotlin/org/radarbase/googlehealth/user/GoogleHealthUserRepository.kt
@@ -27,4 +27,6 @@ abstract class GoogleHealthUserRepository : UserRepository {
     abstract fun getOAuth2AccessToken(user: User): String
 
     abstract fun deregisterUser(serviceUserId: String)
+
+    abstract fun fetchUnauthorizedUsers(): List<User>
 }
