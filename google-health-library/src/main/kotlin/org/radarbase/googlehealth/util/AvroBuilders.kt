6301b49f905f1a37f54f48dd89eb876b39682217
this-Aditya <adityamishra2217@gmail.com>
2026-06-27 16:22:18 +0100

    One more try for avro builders

--- google-health-library/src/main/kotlin/org/radarbase/googlehealth/util/AvroBuilders.kt
+++ google-health-library/src/main/kotlin/org/radarbase/googlehealth/util/AvroBuilders.kt
@@ -13,38 +13,74 @@ import org.radarcns.connector.fitbit.FitbitSkinTemperature
 import org.radarcns.connector.fitbit.FitbitSleepClassic
 import org.radarcns.connector.fitbit.FitbitSleepStage
 
-inline fun intradaySteps(block: FitbitIntradaySteps.Builder.() -> Unit): FitbitIntradaySteps =
-    FitbitIntradaySteps.newBuilder().apply(block).build()
+inline fun intradaySteps(
+    block: FitbitIntradaySteps.Builder.() -> Unit,
+): FitbitIntradaySteps {
+    return FitbitIntradaySteps.newBuilder().apply(block).build()
+}
 
-inline fun intradayHeartRate(block: FitbitIntradayHeartRate.Builder.() -> Unit): FitbitIntradayHeartRate =
-    FitbitIntradayHeartRate.newBuilder().apply(block).build()
+inline fun intradayHeartRate(
+    block: FitbitIntradayHeartRate.Builder.() -> Unit,
+): FitbitIntradayHeartRate {
+    return FitbitIntradayHeartRate.newBuilder().apply(block).build()
+}
 
-inline fun intradayHeartRateVariability(block: FitbitIntradayHeartRateVariability.Builder.() -> Unit): FitbitIntradayHeartRateVariability =
-    FitbitIntradayHeartRateVariability.newBuilder().apply(block).build()
+inline fun intradayHeartRateVariability(
+    block: FitbitIntradayHeartRateVariability.Builder.() -> Unit,
+): FitbitIntradayHeartRateVariability {
+    return FitbitIntradayHeartRateVariability.newBuilder().apply(block).build()
+}
 
-inline fun intradaySpo2(block: FitbitIntradaySpo2.Builder.() -> Unit): FitbitIntradaySpo2 =
-    FitbitIntradaySpo2.newBuilder().apply(block).build()
+inline fun intradaySpo2(
+    block: FitbitIntradaySpo2.Builder.() -> Unit,
+): FitbitIntradaySpo2 {
+    return FitbitIntradaySpo2.newBuilder().apply(block).build()
+}
 
-inline fun restingHeartRate(block: FitbitRestingHeartRate.Builder.() -> Unit): FitbitRestingHeartRate =
-    FitbitRestingHeartRate.newBuilder().apply(block).build()
+inline fun restingHeartRate(
+    block: FitbitRestingHeartRate.Builder.() -> Unit,
+): FitbitRestingHeartRate {
+    return FitbitRestingHeartRate.newBuilder().apply(block).build()
+}
 
-inline fun breathingRate(block: FitbitBreathingRate.Builder.() -> Unit): FitbitBreathingRate =
-    FitbitBreathingRate.newBuilder().apply(block).build()
+inline fun breathingRate(
+    block: FitbitBreathingRate.Builder.() -> Unit,
+): FitbitBreathingRate {
+    return FitbitBreathingRate.newBuilder().apply(block).build()
+}
 
-inline fun skinTemperature(block: FitbitSkinTemperature.Builder.() -> Unit): FitbitSkinTemperature =
-    FitbitSkinTemperature.newBuilder().apply(block).build()
+inline fun skinTemperature(
+    block: FitbitSkinTemperature.Builder.() -> Unit,
+): FitbitSkinTemperature {
+    return FitbitSkinTemperature.newBuilder().apply(block).build()
+}
 
-inline fun sleepClassic(block: FitbitSleepClassic.Builder.() -> Unit): FitbitSleepClassic =
-    FitbitSleepClassic.newBuilder().apply(block).build()
+inline fun sleepClassic(
+    block: FitbitSleepClassic.Builder.() -> Unit,
+): FitbitSleepClassic {
+    return FitbitSleepClassic.newBuilder().apply(block).build()
+}
 
-inline fun sleepStage(block: FitbitSleepStage.Builder.() -> Unit): FitbitSleepStage =
-    FitbitSleepStage.newBuilder().apply(block).build()
+inline fun sleepStage(
+    block: FitbitSleepStage.Builder.() -> Unit,
+): FitbitSleepStage {
+    return FitbitSleepStage.newBuilder().apply(block).build()
+}
 
-inline fun activityLogRecord(block: FitbitActivityLogRecord.Builder.() -> Unit): FitbitActivityLogRecord =
-    FitbitActivityLogRecord.newBuilder().apply(block).build()
+inline fun activityLogRecord(
+    block: FitbitActivityLogRecord.Builder.() -> Unit,
+): FitbitActivityLogRecord {
+    return FitbitActivityLogRecord.newBuilder().apply(block).build()
+}
 
-inline fun activityHeartRate(block: FitbitActivityHeartRate.Builder.() -> Unit): FitbitActivityHeartRate =
-    FitbitActivityHeartRate.newBuilder().apply(block).build()
+inline fun activityHeartRate(
+    block: FitbitActivityHeartRate.Builder.() -> Unit,
+): FitbitActivityHeartRate {
+    return FitbitActivityHeartRate.newBuilder().apply(block).build()
+}
 
-inline fun intradayCalories(block: FitbitIntradayCalories.Builder.() -> Unit): FitbitIntradayCalories =
-    FitbitIntradayCalories.newBuilder().apply(block).build()
+inline fun intradayCalories(
+    block: FitbitIntradayCalories.Builder.() -> Unit,
+): FitbitIntradayCalories {
+    return FitbitIntradayCalories.newBuilder().apply(block).build()
+}
