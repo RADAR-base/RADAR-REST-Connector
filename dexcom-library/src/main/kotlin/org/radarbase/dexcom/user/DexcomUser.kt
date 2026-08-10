59c8db117080c0ff21a5f809e9676f272a617041
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-08-10 19:52:00 +0800

    feat: dexcomUser and User not found file

---
+++ dexcom-library/src/main/kotlin/org/radarbase/dexcom/user/DexcomUser.kt
@@ -0,0 +1,28 @@
+package org.radarbase.dexcom.user
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+import org.radarcns.kafka.ObservationKey
+import java.time.Instant
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class DexcomUser(
+    @JsonProperty("id") override val id: String,
+    @JsonProperty("createdAt") override val createdAt: Instant,
+    @JsonProperty("projectId") override val projectId: String,
+    @JsonProperty("userId") override val userId: String,
+    @JsonProperty("humanReadableUserId") override val humanReadableUserId: String?,
+    @JsonProperty("sourceId") override val sourceId: String,
+    @JsonProperty("externalId") override val externalId: String?,
+    @JsonProperty("isAuthorized") override val isAuthorized: Boolean,
+    @JsonProperty("startDate") override val startDate: Instant,
+    @JsonProperty("endDate") override val endDate: Instant? = null,
+    @JsonProperty("version") override val version: String? = null,
+    @JsonProperty("serviceUserId") override val serviceUserId: String? = null,
+) : User {
+    override val observationKey: ObservationKey = ObservationKey(projectId, userId, sourceId)
+    override val versionedId: String = "$id${version?.let { "#$it" } ?: ""}"
+
+    fun isComplete() =
+        isAuthorized && (endDate == null || startDate.isBefore(endDate)) && serviceUserId != null
+}
