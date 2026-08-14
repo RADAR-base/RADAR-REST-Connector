fd1e110408b028fde987ab125ab4c6bc14323e16
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-08-14 13:46:53 +0800

    feat: added sequence extension

--- dexcom-library/src/main/kotlin/org/radarbase/dexcom/converter/SequenceExtensions.kt
+++ dexcom-library/src/main/kotlin/org/radarbase/dexcom/converter/SequenceExtensions.kt
@@ -4,13 +4,13 @@ import org.slf4j.LoggerFactory
 
 val logger = LoggerFactory.getLogger("org.radarbase.oura.converter.SequenceExtensions")
 
-internal fun <S, T> Sequence<T>.mapCatching(fn: (T) -> S): Sequence<Result<S>> = map { t ->
+fun <S, T> Sequence<T>.mapCatching(fn: (T) -> S): Sequence<Result<S>> = map { t ->
     runCatching {
         fn(t)
     }
 }
 
-internal fun <S, T> Sequence<T>.mapIndexedCatching(fn: (index: Int, T) -> S): Sequence<Result<S>> =
+fun <S, T> Sequence<T>.mapIndexedCatching(fn: (index: Int, T) -> S): Sequence<Result<S>> =
     mapIndexed { index, t ->
         runCatching {
             fn(index, t)
