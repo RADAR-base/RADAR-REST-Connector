06edfc09d1153de19ceeebf76179e22dc6f7fa07
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-08-14 15:21:54 +0800

    feat: dexcom daterange

--- dexcom-library/src/main/kotlin/org/radarbase/dexcom/route/DexcomRouteFactory.kt
+++ dexcom-library/src/main/kotlin/org/radarbase/dexcom/route/DexcomRouteFactory.kt
@@ -11,6 +11,7 @@ object DexcomRouteFactory {
             DexcomCalibrationsRoute(userRepository),
             DexcomAlertsRoute(userRepository),
             DexcomDevicesRoute(userRepository),
+            DexcomDataRangeRoute(userRepository),
         )
     }
 }
