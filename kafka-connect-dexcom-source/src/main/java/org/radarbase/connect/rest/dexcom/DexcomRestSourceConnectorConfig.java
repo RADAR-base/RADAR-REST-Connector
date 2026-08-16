60f4f3d8608477862b28b077a9f23fbe62487442
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-08-16 16:07:50 +0800

    feat: dexcom source task

--- kafka-connect-dexcom-source/src/main/java/org/radarbase/connect/rest/dexcom/DexcomRestSourceConnectorConfig.java
+++ kafka-connect-dexcom-source/src/main/java/org/radarbase/connect/rest/dexcom/DexcomRestSourceConnectorConfig.java
@@ -1,5 +1,6 @@
 package org.radarbase.connect.rest.dexcom;
 
+import java.lang.reflect.InvocationTargetException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Collections;
@@ -11,7 +12,10 @@ import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.config.ConfigException;
+import org.apache.kafka.connect.errors.ConnectException;
 import org.radarbase.connect.rest.dexcom.user.DexcomServiceUserRepository;
+import org.radarbase.connect.rest.dexcom.user.DexcomUserRepository;
+import org.radarbase.dexcom.route.DexcomRoute;
 
 public class DexcomRestSourceConnectorConfig extends AbstractConfig {
 
@@ -29,12 +33,22 @@ public class DexcomRestSourceConnectorConfig extends AbstractConfig {
   public static final String DEXCOM_USER_REPOSITORY_TOKEN_URL_CONFIG =
       "dexcom.user.repository.oauth2.token.url";
 
+  private static final String USERS_SELF_PATH = "/v3/users/self";
+
+  private DexcomUserRepository userRepository;
+
   public DexcomRestSourceConnectorConfig(Map<?, ?> originals) {
     super(conf(), originals);
   }
 
   public static ConfigDef conf() {
     return new ConfigDef()
+        .define(
+            SOURCE_URL_CONFIG,
+            Type.STRING,
+            DexcomRoute.DEFAULT_API_BASE_URL,
+            Importance.HIGH,
+            "Dexcom API base URL (host or full .../v3/users/self path).")
         .define(DEXCOM_USERS_CONFIG, Type.LIST, Collections.emptyList(), Importance.HIGH, "...")
         .define(
             DEXCOM_USER_REPOSITORY_CONFIG,
@@ -99,5 +113,52 @@ public class DexcomRestSourceConnectorConfig extends AbstractConfig {
 
   public boolean getDexcomEgvEnabled() {
     return getBoolean(DEXCOM_EGV_ENABLED_CONFIG);
+  }
+
+  /**
+   * Base URL passed into Dexcom routes. Accepts either a host ({@code
+   * https://sandbox-api.dexcom.com}) or a full path ending in {@code /v3/users/self}.
+   */
+  public String getDexcomApiBaseUrl() {
+    String url = getString(SOURCE_URL_CONFIG).trim();
+    while (url.endsWith("/")) {
+      url = url.substring(0, url.length() - 1);
+    }
+    if (!url.endsWith(USERS_SELF_PATH)) {
+      url = url + USERS_SELF_PATH;
+    }
+    return url;
+  }
+
+  public DexcomUserRepository getUserRepository(DexcomUserRepository reuse) {
+    if (reuse != null && reuse.getClass().equals(getClass(DEXCOM_USER_REPOSITORY_CONFIG))) {
+      userRepository = reuse;
+    } else {
+      userRepository = createUserRepository();
+    }
+    userRepository.initialize(this);
+    return userRepository;
+  }
+
+  public DexcomUserRepository getUserRepository() {
+    if (userRepository == null) {
+      userRepository = createUserRepository();
+    }
+    userRepository.initialize(this);
+    return userRepository;
+  }
+
+  @SuppressWarnings("unchecked")
+  public DexcomUserRepository createUserRepository() {
+    try {
+      return ((Class<? extends DexcomUserRepository>) getClass(DEXCOM_USER_REPOSITORY_CONFIG))
+          .getDeclaredConstructor()
+          .newInstance();
+    } catch (IllegalAccessException
+        | InstantiationException
+        | InvocationTargetException
+        | NoSuchMethodException e) {
+      throw new ConnectException("Invalid user repository class. " + e);
+    }
   }
 }
