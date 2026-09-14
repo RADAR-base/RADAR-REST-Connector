2eed09ff4d3c9ce228b21ba776d9471fa83acd82
Chin-Erdene Gantulga <81845324+Chin-eng@users.noreply.github.com>
2026-09-14 20:38:08 +0800

    fix: dexcom data range

--- dexcom-library/src/main/kotlin/org/radarbase/dexcom/converter/DexcomDataRangeConverter.kt
+++ dexcom-library/src/main/kotlin/org/radarbase/dexcom/converter/DexcomDataRangeConverter.kt
@@ -4,14 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import org.radarbase.dexcom.user.User
 import org.radarcns.connector.dexcom.DexcomDataRange
 import org.radarcns.connector.dexcom.DexcomDataRangeMoment
-import org.radarcns.connector.dexcom.DexcomDataRangeMomentEnd
-import org.radarcns.connector.dexcom.DexcomDataRangeMomentEgvsEnd
-import org.radarcns.connector.dexcom.DexcomDataRangeMomentEgvsStart
-import org.radarcns.connector.dexcom.DexcomDataRangeMomentEventsEnd
-import org.radarcns.connector.dexcom.DexcomDataRangeMomentEventsStart
 import org.radarcns.connector.dexcom.DexcomDataRangeWindow
-import org.radarcns.connector.dexcom.DexcomDataRangeWindowEgvs
-import org.radarcns.connector.dexcom.DexcomDataRangeWindowEvents
 import java.time.Instant
 
 /**
@@ -49,62 +42,20 @@ class DexcomDataRangeConverter(
 
     private fun JsonNode.toDexcomDataRange(): DexcomDataRange =
         DexcomDataRange.newBuilder().apply {
-            calibrations = get("calibrations")?.takeIf { !it.isNull }?.toCalibrationsWindow()
-            egvs = get("egvs")?.takeIf { !it.isNull }?.toEgvsWindow()
-            events = get("events")?.takeIf { !it.isNull }?.toEventsWindow()
+            calibrations = get("calibrations")?.takeIf { !it.isNull }?.toWindow()
+            egvs = get("egvs")?.takeIf { !it.isNull }?.toWindow()
+            events = get("events")?.takeIf { !it.isNull }?.toWindow()
             timeReceived = System.currentTimeMillis() / 1000.0
         }.build()
 
-    private fun JsonNode.toCalibrationsWindow(): DexcomDataRangeWindow =
+    private fun JsonNode.toWindow(): DexcomDataRangeWindow =
         DexcomDataRangeWindow.newBuilder().apply {
-            start = get("start")?.toMoment() ?: emptyMoment()
-            end = get("end")?.toMomentEnd() ?: emptyMomentEnd()
-        }.build()
-
-    private fun JsonNode.toEgvsWindow(): DexcomDataRangeWindowEgvs =
-        DexcomDataRangeWindowEgvs.newBuilder().apply {
-            start = get("start")?.toEgvsStart() ?: emptyEgvsStart()
-            end = get("end")?.toEgvsEnd() ?: emptyEgvsEnd()
-        }.build()
-
-    private fun JsonNode.toEventsWindow(): DexcomDataRangeWindowEvents =
-        DexcomDataRangeWindowEvents.newBuilder().apply {
-            start = get("start")?.toEventsStart() ?: emptyEventsStart()
-            end = get("end")?.toEventsEnd() ?: emptyEventsEnd()
+            start = get("start")?.takeIf { !it.isNull }?.toMoment() ?: emptyMoment()
+            end = get("end")?.takeIf { !it.isNull }?.toMoment() ?: emptyMoment()
         }.build()
 
     private fun JsonNode.toMoment(): DexcomDataRangeMoment =
         DexcomDataRangeMoment.newBuilder().apply {
-            systemTime = systemTimeEpochOrNull()
-            displayTime = textOrNull("displayTime")
-        }.build()
-
-    private fun JsonNode.toMomentEnd(): DexcomDataRangeMomentEnd =
-        DexcomDataRangeMomentEnd.newBuilder().apply {
-            systemTime = systemTimeEpochOrNull()
-            displayTime = textOrNull("displayTime")
-        }.build()
-
-    private fun JsonNode.toEgvsStart(): DexcomDataRangeMomentEgvsStart =
-        DexcomDataRangeMomentEgvsStart.newBuilder().apply {
-            systemTime = systemTimeEpochOrNull()
-            displayTime = textOrNull("displayTime")
-        }.build()
-
-    private fun JsonNode.toEgvsEnd(): DexcomDataRangeMomentEgvsEnd =
-        DexcomDataRangeMomentEgvsEnd.newBuilder().apply {
-            systemTime = systemTimeEpochOrNull()
-            displayTime = textOrNull("displayTime")
-        }.build()
-
-    private fun JsonNode.toEventsStart(): DexcomDataRangeMomentEventsStart =
-        DexcomDataRangeMomentEventsStart.newBuilder().apply {
-            systemTime = systemTimeEpochOrNull()
-            displayTime = textOrNull("displayTime")
-        }.build()
-
-    private fun JsonNode.toEventsEnd(): DexcomDataRangeMomentEventsEnd =
-        DexcomDataRangeMomentEventsEnd.newBuilder().apply {
             systemTime = systemTimeEpochOrNull()
             displayTime = textOrNull("displayTime")
         }.build()
@@ -124,19 +75,4 @@ class DexcomDataRangeConverter(
 
     private fun emptyMoment() =
         DexcomDataRangeMoment.newBuilder().build()
-
-    private fun emptyMomentEnd() =
-        DexcomDataRangeMomentEnd.newBuilder().build()
-
-    private fun emptyEgvsStart() =
-        DexcomDataRangeMomentEgvsStart.newBuilder().build()
-
-    private fun emptyEgvsEnd() =
-        DexcomDataRangeMomentEgvsEnd.newBuilder().build()
-
-    private fun emptyEventsStart() =
-        DexcomDataRangeMomentEventsStart.newBuilder().build()
-
-    private fun emptyEventsEnd() =
-        DexcomDataRangeMomentEventsEnd.newBuilder().build()
 }
